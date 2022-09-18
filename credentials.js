97b28d30779e9ef48a8d2a9df3eb48e88b1bd81e
Theresia Tawk <theresiatawk@hotmail.com>
2022-09-18 16:22:57 +0300

    Getting the score from local Storage

--- credentials.js
+++ credentials.js
@@ -3,17 +3,18 @@ const signUp = e => {
         lname = document.getElementById('lname').value,
         email = document.getElementById('email').value,
         pwd = document.getElementById('pwd').value;
-
+        score = 0;
+  
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
-
+  
     let exist = formData.length && 
         JSON.parse(localStorage.getItem('formData')).some(data => 
             data.fname.toLowerCase() == fname.toLowerCase() && 
             data.lname.toLowerCase() == lname.toLowerCase()
         );
-
+  
     if(!exist){
-        formData.push({ fname, lname, email, pwd });
+        formData.push({ fname, lname, email, pwd, score });
         localStorage.setItem('formData', JSON.stringify(formData));
         document.querySelector('form').reset();
         document.getElementById('fname').focus();
@@ -23,19 +24,24 @@ const signUp = e => {
         alert("Ooopppssss... Duplicate found!!!\nYou have already signed up");
     }
     e.preventDefault();
-}
-
-function signIn(e) {
+  }
+  
+  function signIn(e) {
     let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
     let exist = formData.length && 
     JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    console.log("Ana Barrra");
     if(!exist){
         alert("Incorrect login credentials");
     }
     else{
+        let userIndex = JSON.parse(localStorage.getItem('formData')).findIndex(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+        let users = JSON.parse(localStorage.getItem('formData'));
+        let loginUser = users[userIndex];
+        let score = loginUser.score;
+        localStorage.setItem("score", score);
         location.href = "./game.html";
-        console.log(localStorage);
     }
     e.preventDefault();
-}
+  }
--- game.js
+++ game.js
@@ -1,66 +1,62 @@
-let gameIsOn = false;
-let score = 0;
-
-const gameStart = (value) => {
-  if (value === "start") {
-    console.log("GAME START");
-    defaultCase();
-    console.log(score);
-    gameIsOn = true;
-  }
-};
-
-const gameEnd = () => {
-  gameIsOn = false;
-};
-
-const loosingCase = () => {
-  score = score - 10;
+window.onload = () => {
+  let gameIsOn = false;
   const walls = document.getElementsByClassName("boundary");
   const game_status = document.getElementById("status");
-  for (let i = 0; i < walls.length; i++) {
-    walls[i].style.backgroundColor = "#ff8888";
-  }
-  game_status.innerText = "You Lost. You score is now "+ score;
-  game_status.style.color = "red";
-};
+  // console.log(localStorage);
+  let score = localStorage.getItem("score");
 
-const defaultCase = () => {
-  const walls = document.getElementsByClassName("boundary");
-  let game_status = document.getElementById("status");
+  const gameStart = (value) => {
+    if (value === "start") {
+      console.log("GAME START");
+      defaultCase();
+      gameIsOn = true;
+    }
+  };
+
+  const gameEnd = () => {
+    gameIsOn = false;
+  };
+
+  const loosingCase = () => {
+    score = score - 10;
+    for (let i = 0; i < walls.length; i++) {
+      walls[i].style.backgroundColor = "#ff8888";
+    }
+    game_status.innerText = "You Lost. You score is now " + score;
+    game_status.style.color = "red";
+  };
+
+  const defaultCase = () => {
     for (let i = 0; i < walls.length; i++) {
       walls[i].style.backgroundColor = "#eeeeee";
-  }
-  game_status.innerText = "Begin by moving your mouse over the \"S\". You current score is "+ score;
-  game_status.style.color = "black";
-};
+    }
+    game_status.innerText = 'Begin by moving your mouse over the "S".';
+    game_status.style.color = "black";
+  };
 
-const winningCase = () => {
-  score = score + 5;
-  const walls = document.getElementsByClassName("boundary");
-  const game_status = document.getElementById("status");
-  for (let i = 0; i < walls.length; i++) {
-    walls[i].style.backgroundColor = "#eeeeee";
-  }
-  game_status.innerText = "You Won. You score is now "+ score;
-  game_status.style.color = "red";
-};
+  const winningCase = () => {
+    score = score + 5;
+    for (let i = 0; i < walls.length; i++) {
+      walls[i].style.backgroundColor = "#eeeeee";
+    }
+    game_status.innerText = "You Won. You score is now " + score;
+    game_status.style.color = "red";
+  };
 
-const collisionCheck = (value) => {
-  if (value === "" || value === "boundary1") {
-    console.log("You Lost");
-    loosingCase();
-    gameEnd();
-  } else if (value === "end") {
-    winningCase();
-    gameEnd();
-  }
-};
+  const collisionCheck = (value) => {
+    if (value === "" || value === "boundary1") {
+      console.log("You Lost");
+      loosingCase();
+      gameEnd();
+    } else if (value === "end") {
+      winningCase();
+      gameEnd();
+    }
+  };
 
-window.onload = () => {
   window.addEventListener("mousemove", (e) => {
     let value = e.target.id;
-    console.log(value);
+    // console.log(value);
     gameStart(value);
     if (gameIsOn) {
       collisionCheck(value);
