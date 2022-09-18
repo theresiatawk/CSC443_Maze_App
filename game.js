6fdd7a023a238d95b2f93a5bff5bcc953b35f23c
Theresia Tawk <theresiatawk@hotmail.com>
2022-09-18 12:19:37 +0300

    Fixing Bugs

--- game.js
+++ game.js
@@ -1,46 +1,57 @@
 let gameIsOn = false;
-let userLost = false;
 let score = 0;
 
 const gameStart = (value) => {
   if (value === "start") {
     console.log("GAME START");
+    defaultCase();
     gameIsOn = true;
-    const walls = document.getElementsByClassName("boundary");
-    const game_status = document.getElementById("status");
-    for (let i = 0; i < walls.length; i++) {
-      walls[i].style.backgroundColor = "#eeeeee";
-    }
-    game_status.innerText = "Begin by moving your mouse over the \"S\". You current score is "+score;
-    game_status.style.color = "black";
   }
 };
 
 const gameEnd = () => {
   gameIsOn = false;
-  if (userLost) {
-    const walls = document.getElementsByClassName("boundary");
-    const game_status = document.getElementById("status");
-    for (let i = 0; i < walls.length; i++) {
-      walls[i].style.backgroundColor = "#ff8888";
-    }
-    game_status.innerText = "You Lost. You score is now "+ score;
-    game_status.style.color = "red";
+};
+
+const loosingCase = () => {
+  score = score - 10;
+  const walls = document.getElementsByClassName("boundary");
+  const game_status = document.getElementById("status");
+  for (let i = 0; i < walls.length; i++) {
+    walls[i].style.backgroundColor = "#ff8888";
   }
+  game_status.innerText = "You Lost. You score is now "+ score;
+  game_status.style.color = "red";
+};
+
+const defaultCase = () => {
+  const walls = document.getElementsByClassName("boundary");
+  let game_status = document.getElementById("status");
+    for (let i = 0; i < walls.length; i++) {
+      walls[i].style.backgroundColor = "#eeeeee";
+  }
+  game_status.innerText = "Begin by moving your mouse over the \"S\". You current score is ";
+  game_status.style.color = "black";
+};
+
+const winningCase = () => {
+  score = score + 5;
+  const walls = document.getElementsByClassName("boundary");
+  const game_status = document.getElementById("status");
+  for (let i = 0; i < walls.length; i++) {
+    walls[i].style.backgroundColor = "#eeeeee";
+  }
+  game_status.innerText = "You Won. You score is now "+ score;
+  game_status.style.color = "red";
 };
 
 const collisionCheck = (value) => {
   if (value === "" || value === "boundary1") {
     console.log("You Lost");
-    userLost = true;
-    score -= 10;
+    loosingCase();
     gameEnd();
   } else if (value === "end") {
-    score += 5;
-    const game_status = document.getElementById("status");
-    game_status.innerText = "You Won. Your score is now "+score;
-    game_status.style.color = "red";
-    userLost = false;
+    winningCase();
     gameEnd();
   }
 };
